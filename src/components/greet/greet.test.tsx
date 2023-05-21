5990b5633630f85dd6bb9e4d6e7eea632cc67db7
amontalvof <amontalvof92@gmail.com>
2023-05-21 16:28:01 -0400

    feat: Test Driven Development

--- src/components/greet/greet.test.tsx
+++ src/components/greet/greet.test.tsx
@@ -1,8 +1,16 @@
 import { render, screen } from '@testing-library/react';
 import Greet from '.';
 
-test('Greet renders correctly', () => {
-    render(<Greet />);
-    const textElement = screen.getByText(/hello/i);
-    expect(textElement).toBeInTheDocument();
+describe('Greet', () => {
+    test('should Greet renders correctly', () => {
+        render(<Greet />);
+        const textElement = screen.getByText('Hello');
+        expect(textElement).toBeInTheDocument();
+    });
+
+    test('should Greet renders with a name', () => {
+        render(<Greet name="Andy" />);
+        const textElement = screen.getByText('Hello Andy');
+        expect(textElement).toBeInTheDocument();
+    });
 });
