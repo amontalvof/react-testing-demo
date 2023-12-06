2dc12d9c5acfeaf3a04b1c1d691b50fcbd4e1f8e
amontalvof <amontalvof92@gmail.com>
2023-12-05 21:16:31 -0500

    feat: Code Coverage

--- src/components/greet/greet.test.tsx
+++ src/components/greet/greet.test.tsx
@@ -1,16 +1,16 @@
 import { render, screen } from '@testing-library/react';
-import Greet from '.';
+import Greet from './greet';
 
 describe('Greet', () => {
-    test.skip('should Greet renders correctly', () => {
+    test('should Greet renders correctly', () => {
         render(<Greet />);
-        const textElement = screen.getByText('Hello');
+        const textElement = screen.getByText(/Hello/);
         expect(textElement).toBeInTheDocument();
     });
 
-    test.only('should Greet renders a name', () => {
-        render(<Greet name="Andy" />);
-        const textElement = screen.getByText('Hello Andy');
-        expect(textElement).toBeInTheDocument();
-    });
+    // test('should Greet renders a name', () => {
+    //     render(<Greet name="Andy" />);
+    //     const textElement = screen.getByText('Hello Andy');
+    //     expect(textElement).toBeInTheDocument();
+    // });
 });
--- src/components/greet/index.tsx
+++
@@ -1,9 +0,0 @@
-type GreetProps = {
-    name?: string;
-};
-
-const Greet = ({ name }: GreetProps) => {
-    return <div>Hello {name}</div>;
-};
-
-export default Greet;
