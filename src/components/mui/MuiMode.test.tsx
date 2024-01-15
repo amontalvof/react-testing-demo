3fb12f03f662a08e27df858e21d507918c376992
amontalvof <amontalvof92@gmail.com>
2024-01-14 19:47:59 -0500

    feat: Custom Render Functions

--- src/components/mui/MuiMode.test.tsx
+++ src/components/mui/MuiMode.test.tsx
@@ -1,10 +1,9 @@
-import { render, screen } from '@testing-library/react';
 import { MuiMode } from './MuiMode';
-import { AppProviders } from '../../providers/AppProviders';
+import { render, screen } from '../../test-utils';
 
 describe('MuiMode', () => {
     test('renders text correctly', () => {
-        render(<MuiMode />, { wrapper: AppProviders });
+        render(<MuiMode />);
         const headingElement = screen.getByRole('heading');
         expect(headingElement).toHaveTextContent('dark mode');
     });
